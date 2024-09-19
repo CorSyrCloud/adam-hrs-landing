97874534c5f5e029b532e737371c1e73278b953a
CorSyrCloud <166336862+CorSyrCloud@users.noreply.github.com>
2024-09-19 11:16:56 -0300

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 // NAVIGATION
 export const NAV_LINKS = [
   { href: "/", key: "home", label: "Home" },
-  { href: "/", key: "features", label: "Feaures" },
+  { href: "/", key: "features", label: "Features" },
   { href: "/", key: "services", label: "Services" },
   { href: "/", key: "pricing ", label: "Pricing " },
   { href: "/", key: "contact_us", label: "Contact Us" },
